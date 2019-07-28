a7dc007456d14758962d0af987f609d1ad5caacd
Clint.Network <me@clint.network>
2019-07-28 02:19:38 +0200

    Call Lucid from the main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+/**
+ * Description: High performance and distributed KV ledger.
+ * Author: by Clint.Network
+ * Date: 28/07/2019
+*/
+
+#[macro_use] extern crate nickel;
+
+mod lucid;
+use lucid::Lucid;
+
 fn main()
 {
+    Lucid::init()
+        .banner()
+        .commandline();
 }
