96a5a32e839b17cd6c9c79dcd2e62e6788340252
Clint.Network <me@clint.network>
2019-09-22 17:14:34 +0200

    Add errors display in logger

--- src/logger.rs
+++ src/logger.rs
@@ -1,4 +1,4 @@
-use colored::*;
+//use colored::*;
 use chrono::{DateTime, Utc};
 
 pub enum LogLevel
@@ -10,25 +10,33 @@ pub enum LogLevel
 }
 
 pub trait Logger {
-    fn log(&self, level: LogLevel, message: &str, _flag: &str);
+    fn log(&self, level: LogLevel, message: &str, err: Option<&str>);
+    fn display(&self, level: &LogLevel, message: &str);
 }
 
 impl<T> Logger for T {
-    fn log(&self, level: LogLevel, message: &str, _flag: &str)
+    fn log(&self, level: LogLevel, message: &str, err: Option<&str>)
     {
+        &self.display(&level, &message);
+        if err.is_some() {
+            &self.display(&level, err.unwrap());
+        }
+    }
+
+    fn display(&self, level: &LogLevel, message: &str) {
         let now = Utc::now().format("%Y/%m/%d %H:%M:%S");
         match level {
             LogLevel::Success => {
                 println!("{} [{}] lucid: {}", now, "SUCCESS", message);
             },
             LogLevel::Information => {
-                println!("{} [{}]    lucid: {}", now, "INFO", message);
+                println!("{} [{}] lucid: {}", now, "INFO", message);
             },
             LogLevel::Warning => {
                 eprintln!("{} [{}] lucid: {}", now, "WARNING", message);
             },
             LogLevel::Error => {
-                eprintln!("{} [{}]   lucid: {}", now, "ERROR", message);
+                eprintln!("{} [{}] lucid: {}", now, "ERROR", message);
             }
         };
     }
