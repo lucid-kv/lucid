7be031ac3dcf477b648a98ba29c9f990665ea11d
rigwild <contact@asauvage.fr>
2019-10-27 15:27:58 +0100

    webui: Triangle loader

--- webui/src/components/Loader.vue
+++ webui/src/components/Loader.vue
@@ -1,6 +1,20 @@
 <template>
-  <span>
-    <b-spinner label="Loading..." />
+  <span id="spinner">
+    <feather type="triangle" animation="spin" animation-speed="fast" />
     <p>Loading...</p>
   </span>
 </template>
+
+<style scoped>
+#spinner {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin: 20px 0px;
+  width: fit-content;
+}
+#spinner p {
+  margin-top: 5px;
+}
+</style>
