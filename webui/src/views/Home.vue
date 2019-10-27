8ef3f232fcab0892c0ac3289f79d3b49c1b5ceb3
rigwild <contact@asauvage.fr>
2019-10-27 14:25:47 +0100

    webui: Main scrollbar style

--- webui/src/views/Home.vue
+++ webui/src/views/Home.vue
@@ -2,6 +2,31 @@
   <div>
     <h1>Home</h1>
     <p>Welcome to Lucid!</p>
+
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
+    <p>Welcome to Lucid!</p>
   </div>
 </template>
 
