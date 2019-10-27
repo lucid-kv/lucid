36fb224657bad9b2b766d12d8c741c2b90f5584d
rigwild <contact@asauvage.fr>
2019-10-27 10:46:35 +0100

    Import bootstrap custom skin

--- webui/src/main.js
+++ webui/src/main.js
@@ -4,11 +4,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// Import Bootstrap + Bootstrap-Vue
 import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 Vue.use(BootstrapVue)
 
+// Import custom Bootstrap skin
+import './main.css'
+
+// Import Vue-Promised
 import { Promised } from 'vue-promised'
 Vue.component('Promised', Promised)
 
