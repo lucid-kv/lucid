9beca6998085ee4f571a9ecf0050b4e504d1f0d1
Clint.Network <me@clint.network>
2019-11-11 00:30:33 +0100

    Add some fields to KvElement, handling mime type and updated date time

--- src/kvstore.rs
+++ src/kvstore.rs
@@ -1,11 +1,13 @@
 use chashmap::CHashMap;
+use chrono::{DateTime, Utc};
 
-//use chrono::{DateTime, Utc};
-
+#[derive(Debug, Clone)]
 pub struct KvElement {
-    data: Vec<u8>,
-//    created: DateTime<Utc>,
-//    locked: bool
+    pub data: Vec<u8>,
+    pub mime: String,
+    pub created: DateTime<Utc>,
+    pub updated: DateTime<Utc>,
+    pub locked: bool
 }
 
 pub struct KvStore {
@@ -24,17 +26,33 @@ impl KvStore
 
     pub fn set(&self, key: String, value: Vec<u8>) -> Option<KvElement> {
         // TODO: prepare iterative persistence
-        self.container.insert(key, KvElement { data: value })
+        match &mut self.container.get_mut(&key) {
+            Some(kv_element) => {
+                kv_element.updated = Utc::now();
+                Some(kv_element.to_owned())
+            },
+            None => {
+                let mime_type = tree_magic::from_u8(value.as_ref());
+                let kv_element = KvElement {
+                    data: value,
+                    mime: mime_type,
+                    created: Utc::now(),
+                    updated: Utc::now(),
+                    locked: false,
+                };
+                self.container.insert(key, kv_element)
+            }
+        }
     }
 
     pub fn get(&self, key: String) -> Option<Vec<u8>> {
-        match (&self.container).get(&key) {
+        match &self.container.get(&key) {
             Some(value) => Some((&value.data).clone()),
             None => None
         }
     }
 
-    // TODO: implement Lock, Unlock, Increment, Decrement
+    // TODO: implement Lock, Unlock, Increment, Decrement, Expire
 
     pub fn drop(&self, key: String) {
         &self.container.remove(&key);
