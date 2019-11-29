e67b2201c7407d5d898bf4534829a61e50c21fef
Clint.Network <me@clint.network>
2019-11-29 16:10:18 +0100

    Add development credits

--- src/lucid.rs
+++ src/lucid.rs
@@ -40,7 +40,16 @@ impl Lucid {
     }
 
     fn show_version(&self) {
-        println!("Lucid Version {}\n", crate_version!());
+        info!("Lucid Version {}\n", crate_version!());
+        println!("\
++-----------------+-----------------------+--------------------+\n\
+|               Lucid KV Development Credits                   |\n\
++-----------------+-----------------------+--------------------+\n\
+| Clint Mourlevat | me@clint.network      | Lucid Founder      |\n\
+| Jonathan Serra  | jonathan@blocs.fr     | Core Development   |\n\
+| CephalonRho     | CephalonRho@gmail.com | Core Development   |\n\
+| Rigwild         | me@rigwild.dev        | Web UI Development |\n\
++-----------------+-----------------------+--------------------+")
     }
 
     fn show_help(&self, commands: &mut App) {
