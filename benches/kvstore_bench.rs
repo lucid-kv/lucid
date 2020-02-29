81f1ef22245361349473641d6da03f77bd626adb
Blocs <jonathan@cryptoblocs.fr>
2020-02-29 05:14:41 +0100

    Fixes Bench

--- benches/kvstore_bench.rs
+++ benches/kvstore_bench.rs
@@ -2,27 +2,27 @@ use criterion::{criterion_group, criterion_main, Criterion};
 
 use lucid::kvstore::KvStore;
 
-fn set_1_kb_data_without_encryption(c: &mut Criterion) {
+fn set_10_kb_data_without_encryption(c: &mut Criterion) {
     let kv = KvStore::new(None);
 
-    let data = [42u8; 1000];
+    let data = [42u8; 10000];
 
-    c.bench_function("Set 1KB (no encrytion)", |b| {
+    c.bench_function("Set 10KB (no encrytion)", |b| {
         b.iter(|| kv.set("bench_one".to_string(), data.to_vec()))
     });
 }
-fn set_1_kb_data(c: &mut Criterion) {
+fn set_10_kb_data(c: &mut Criterion) {
     let kv = KvStore::new(Some([
         "123456789012345678901234123456789012345678901234",
         "f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff",
     ]));
 
-    let data = [42u8; 1000];
+    let data = [42u8; 10000];
 
-    c.bench_function("Set 1KB", |b| {
+    c.bench_function("Set 10KB", |b| {
         b.iter(|| kv.set("bench_one".to_string(), data.to_vec()))
     });
 }
 
-criterion_group!(benches, set_1_kb_data, set_1_kb_data_without_encryption);
+criterion_group!(benches, set_10_kb_data, set_10_kb_data_without_encryption);
 criterion_main!(benches);
